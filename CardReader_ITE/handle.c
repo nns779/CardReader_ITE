fd7c38479b2caac8f395359269428fef90a09de8
nns779 <nns779@users.noreply.github.com>
2017-02-08 21:34:42 +0900

    handle: 関数の追加など

--- CardReader_ITE/handle.c
+++ CardReader_ITE/handle.c
@@ -3,12 +3,14 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
+#include <windows.h>
 
 #include "debug.h"
 #include "memory.h"
 #include "handle.h"
 
 struct handle_list_info {
+	CRITICAL_SECTION sct;
 	uintptr_t base;
 	uint32_t num;
 	handle_release_callback callback;
@@ -28,6 +30,7 @@ bool handle_list_init(handle_list *const h, const uintptr_t base, const uintptr_
 		return false;
 	}
 
+	InitializeCriticalSection(&info->sct);
 	info->base = base;
 	info->num = num;
 	info->callback = callback;
@@ -44,16 +47,31 @@ bool handle_list_deinit(handle_list h)
 
 	for (uint32_t i = 0; i < info->num; i++) {
 		if (info->handle[i] != NULL) {
-			info->callback(info->handle[i]);
+			info->callback(info->handle[i], NULL);
 		}
 	}
 
+	DeleteCriticalSection(&info->sct);
 	memFree(info);
 
 	return true;
 }
 
-bool handle_list_put(handle_list h, void *const p, uintptr_t *const rv)
+void handle_list_lock(handle_list h)
+{
+	struct handle_list_info *info = h;
+
+	EnterCriticalSection(&info->sct);
+}
+
+void handle_list_unlock(handle_list h)
+{
+	struct handle_list_info *info = h;
+
+	LeaveCriticalSection(&info->sct);
+}
+
+bool handle_list_put_nolock(handle_list h, void *const p, uintptr_t *const rv)
 {
 	struct handle_list_info *info = h;
 	uintptr_t id = 0, num = info->num;
@@ -72,22 +90,43 @@ bool handle_list_put(handle_list h, void *const p, uintptr_t *const rv)
 	return false;
 }
 
-bool handle_list_get(handle_list h, const uintptr_t v, void **const rp)
+bool handle_list_put(handle_list h, void *const p, uintptr_t *const rv)
+{
+	bool r;
+
+	handle_list_lock(h);
+	r = handle_list_put_nolock(h, p, rv);
+	handle_list_unlock(h);
+
+	return r;
+}
+
+bool handle_list_get_nolock(handle_list h, const uintptr_t v, void **const rp)
 {
 	struct handle_list_info *info = h;
 	uintptr_t id = (v - info->base);
 
 	*rp = NULL;
 
-	if (id >= info->num)
-		return false;
-
-	*rp = info->handle[id];
+	if (id < info->num) {
+		*rp = info->handle[id];
+	}
 
 	return (*rp != NULL) ? true : false;
 }
 
-bool handle_list_release(handle_list h, const uintptr_t v)
+bool handle_list_get(handle_list h, const uintptr_t v, void **const rp)
+{
+	bool r;
+
+	handle_list_lock(h);
+	r = handle_list_get_nolock(h, v, rp);
+	handle_list_unlock(h);
+
+	return r;
+}
+
+bool handle_list_release_nolock(handle_list h, const uintptr_t v, const bool c, void *prm, uintptr_t *const ret)
 {
 	struct handle_list_info *info = h;
 	uintptr_t id = (v - info->base);
@@ -95,8 +134,27 @@ bool handle_list_release(handle_list h, const uintptr_t v)
 	if (id >= info->num)
 		return false;
 
-	info->callback(info->handle[id]);
+	if (c == true)
+	{
+		uintptr_t r;
+
+		r = info->callback(info->handle[id], prm);
+
+		if (ret != NULL)
+			*ret = r;
+	}
 	info->handle[id] = NULL;
 
 	return true;
+}
+
+bool handle_list_release(handle_list h, const uintptr_t v, const bool c, void *prm, uintptr_t *const ret)
+{
+	bool r;
+
+	handle_list_lock(h);
+	r = handle_list_release_nolock(h, v, c, prm, ret);
+	handle_list_unlock(h);
+
+	return r;
 }
--- CardReader_ITE/handle.h
+++ CardReader_ITE/handle.h
@@ -4,11 +4,16 @@
 
 #include <stdint.h>
 
-typedef void(*handle_release_callback)(void *handle);
+typedef uintptr_t(*handle_release_callback)(void *handle, void *prm);
 typedef void* handle_list;
 
 extern bool handle_list_init(handle_list *const h, const uintptr_t base, const uintptr_t num, const handle_release_callback callback);
 extern bool handle_list_deinit(handle_list h);
+extern void handle_list_lock(handle_list h);
+extern void handle_list_unlock(handle_list h);
+extern bool handle_list_put_nolock(handle_list h, void *const p, uintptr_t *const rv);
 extern bool handle_list_put(handle_list h, void *const p, uintptr_t *const rv);
+extern bool handle_list_get_nolock(handle_list h, const uintptr_t v, void **const rp);
 extern bool handle_list_get(handle_list h, const uintptr_t v, void **const rp);
-extern bool handle_list_release(handle_list h, const uintptr_t v);
+extern bool handle_list_release_nolock(handle_list h, const uintptr_t v, const bool c, void *prm, uintptr_t *const ret);
+extern bool handle_list_release(handle_list h, const uintptr_t v, const bool c, void *prm, uintptr_t *const ret);
